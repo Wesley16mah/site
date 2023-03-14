38d88437083ecc3e4864b5dac5af3fa639e9b968
wesley16mah <125272674+wesley16mah@users.noreply.github.com>
2023-03-14 20:12:27 -0300

    Add files via upload
    codigo

---
+++ app.java
@@ -0,0 +1,36 @@
+import java.util,Scanner;{
+    public class App {
+
+        public static void main(String[] args) thraws Excption {
+            int a=0;
+            Scanner scanner = new Scanner (System.in);
+            do {
+                System.out.println(x: "escolha uma opção:");
+                System.out.println(x: "1 - Iniciar");
+                System.out.println(x: "2 - Ajuda");
+                System.out.prontln(x: "3 - Sair");
+                a = scanner.nextint();
+               // if (a==3){
+                   // System.out.println("Você esscolher sair");
+                //}
+
+                switch (a){
+                    case 1:
+                        System.out.println(x: "Iniciar");
+                        break;
+                    case 2:
+                        System.out.println(x: "Ajuda");
+                        case 3:
+                        System.out.println}(x: 'Saindo....');
+
+                    default:
+                        System.out.println(x: "Tecla inválida");
+                        break;
+
+                }
+
+             } while (a!=3);
+             scanner.close();
+
+    }
+}  
